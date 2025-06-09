2a72688c521903847a70d4b5e0e33a7db0bb04fd
Cillerius <matusmatola149@gmail.com>
2025-06-09 15:02:45 +0200

    attempting to add the setup

--- obj/Release/net8.0-windows/Hexagons.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Hexagons.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hexagons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e685850ff54765505b3b7258083f5b563299f1cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097a0b827b0fa0574e40913b4b96d5d7df408a5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hexagons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hexagons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
